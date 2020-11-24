57d34d673051a18364f7a4dfcdc3b0d45693530d
DongHyeonYu <ydh91026@kookmin.ac.kr>
2020-11-24 23:42:16 +0900

    modified source

--- P_control/P_control.ino
+++ P_control/P_control.ino
@@ -1,4 +1,4 @@
-#include <Servo.h> //[3104] Servo header File include
+#include <Servo.h> 
 
 /////////////////////////////
 // Configurable parameters //
@@ -7,44 +7,51 @@
 float dist_raw2, raw_dist;
 // sensor values
 float x[8] = {70.0, 110.0, 150.0, 175.0, 195.0, 215.0, 245.0, 250.0};
-
+ 
 // real values
 float y[8] = {100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0};
 
+////////////////////////////
+#define DELAY_MICROS  1500
+#define EMA_ALPHA 0.35
+float filtered_dist;
+float ema_dist = 0;
+float samples_num = 3;
+///////////to filter////////
+
 
 // Arduino pin assignment
-#define PIN_LED 9 // [3110] 9번핀 LED 연결  
-#define PIN_SERVO 10 // [3110] 10번핀 서보 연결
-#define PIN_IR A0 //[3104] 적외선 거리센서 PIN - Analog0 정의 
+#define PIN_LED 9   
+#define PIN_SERVO 10 
+#define PIN_IR A0  
 
 // Framework setting
-#define _DIST_TARGET 255 //[3104] 탁구공을 위치 시킬 목표 
-#define _DIST_MIN 100 //[3117] 거리 최소값
-#define _DIST_MAX 410 //[3117] 거리 최대값
+#define _DIST_TARGET 255 
+#define _DIST_MIN 100 
+#define _DIST_MAX 410 
 
 // Distance sensor
-#define _DIST_ALPHA 0.3  //[3099] EMA 필터링을 위한 alpha 값
-               // [3108] 0~1 사이의 값
+#define _DIST_ALPHA 0.3  
 
 // Servo range
-#define _DUTY_MIN 1143   //[3100] 최저 서보 위치 // up
-#define _DUTY_NEU 1500    //[3100] 중립 서보 위치
-#define _DUTY_MAX 1857  //[3100] 최대 서보 위치 //Down
+#define _DUTY_MIN 1243   
+#define _DUTY_NEU 1545   
+#define _DUTY_MAX 1752  
 
 // Servo speed control
-#define _SERVO_ANGLE 50.0 //본인 서보의 실제 구동범위? 
-#define _SERVO_SPEED 50.0
+#define _SERVO_ANGLE 50.0 
+#define _SERVO_SPEED 70.0
 
 // Event periods
-#define _INTERVAL_DIST 15 //[3099] 각 event 사이에 지정한 시간 간격
-#define _INTERVAL_SERVO 10
+#define _INTERVAL_DIST 10
+#define _INTERVAL_SERVO 15
 #define _INTERVAL_SERIAL 20
 
 // PID parameters
-#define _KP 1.0
+#define _KP 1.25
 
 #define a 70
-#define b 280
+#define b 245
 
 //////////////////////
 // global variables //
@@ -54,26 +61,44 @@ float y[8] = {100.0, 150.0, 200.0, 250.0, 300.0, 350.0, 400.0, 450.0};
 Servo myservo;
 
 // Distance sensor
-float dist_target; // location to send the ball
-float dist_raw, dist_ema; //[1928] 측정된 값과 ema 필터를 적용한 값
+float dist_target; 
+float dist_raw, dist_ema; 
 
 // Event periods
 unsigned long last_sampling_time_dist, last_sampling_time_servo, last_sampling_time_serial; 
-//[3104] 각 event의 진행 시간 저장 변수 
 bool event_dist, event_servo, event_serial; 
-//[3104] 각 event의 시간체크를 위한 변수 (ex_20초 주기 >> 0초(True,시작), 10초(False), 20초(True))
 
 // Servo speed control
-int duty_chg_per_interval; // [3116] 주기 당 서보 duty값 변화량
-int duty_target, duty_curr; //[1928] 목표 위치와 현재 위치
+int duty_chg_per_interval;
+int duty_target, duty_curr; 
 
 // PID variables
 float error_curr, error_prev, control, pterm, dterm, iterm;
 
-//error_curr: 현재 측정값과 목표값의 차이
-//error_prev: 직전에 구한 차이로, P제어에서는 사용하지 않을 것임
-//control: PID제어의 결과로 얻은 제어값
-//pterm: Proportional term, 현재 상태의 error값으로부터 얻은 Proportional gain을 저장하는 변수
+float under_noise_filter(void){
+  float currReading;
+  float largestReading = 0;
+  for (int i = 0; i < samples_num; i++) {
+    currReading = ir_distance();
+    if (currReading > largestReading) { largestReading = currReading; }
+    // Delay a short time before taking another reading
+    delayMicroseconds(DELAY_MICROS);
+  }
+  largestReading = 100 + 300.0 / (b - a) * (largestReading - a);
+  return largestReading;
+}
+
+float filtered_ir_distance(void){
+  float currReading;
+  float lowestReading = 1024;
+  for (int i = 0; i < samples_num; i++) {
+    currReading = under_noise_filter();
+    if (currReading < lowestReading) { lowestReading = currReading; }
+  }
+  ema_dist = EMA_ALPHA*lowestReading + (1-EMA_ALPHA)*ema_dist;
+  return ema_dist;
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -118,7 +143,7 @@ void loop() {
 
   if(event_dist){
     event_dist = false;
-    dist_ema = ir_distance_filtered();
+    dist_ema = filtered_ir_distance();
     dist_raw2 = ir_distance();
 
     error_curr = dist_ema - _DIST_TARGET;
@@ -148,20 +173,20 @@ void loop() {
   
       
   if(event_serial) {
-    event_serial = false; 
-    Serial.print("Min:0,Low:200,dist:");
+    event_serial = false;
+     
+    Serial.print("dist_ir:");
     Serial.print(dist_ema);
-    Serial.print("no_filtered:");
-    Serial.print(dist_raw2);
-    Serial.print("dcpi:"); 
-    Serial.print(duty_chg_per_interval);
     Serial.print(",pterm:");
-    Serial.print(pterm);
+    Serial.print(map(pterm,-1000,1000,510,610));
     Serial.print(",duty_target:");
-    Serial.print(duty_target);
+    Serial.print(map(duty_target,1000,2000,410,510));
     Serial.print(",duty_curr:");
-    Serial.print(duty_curr);
-    Serial.println(",High:310,Max:2000");
+    Serial.print(map(duty_curr,1000,2000,410,510));
+    Serial.println(",Min:100,Low:200,dist_target:255,High:310,Max:410");
+    
+    //Serial.print("No_filtered:");
+    //Serial.println(ir_distance());
   }
 }
 
@@ -171,11 +196,13 @@ float ir_distance(void){
   value = ((6762.0/(volt-9.0))-4.0) * 10.0;
   return value;
 }
+/*
 float ir_distance_filtered(void){
-  raw_dist = ir_distance();
+  raw_dist =  ir_distance();
   dist_raw = 100 + 300.0 / (b - a) * (raw_dist - a);
   return _DIST_ALPHA*dist_raw+(1-_DIST_ALPHA)*dist_ema;
 }
+*/
 
 
 float ir_distance_sequence(void){
@@ -197,9 +224,7 @@ float ir_distance_sequence(void){
       break;
     }
   }
-  // out of sequence range: set value to neutral position
-  // 센서 전원 Off 시, 오차값 발생으로 구간에서 벗어났을 시 등 
-   if(s > e){
+  if(s > e){
     if(value > 10000.0 || value < -10000.0) real_value = _DIST_TARGET;
     else if(s == 0) real_value = _DIST_MIN; 
     else real_value = _DIST_MAX;
